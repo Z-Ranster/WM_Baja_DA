86d92b886c47d1e0fe30b4b86385d29568bba3b2
gabsillis <gianniabsillis@gmail.com>
2017-11-21 20:31:59 -0500

    Update WM_2018_CarCode.ino
    added RPM stuffs

--- Onboard/WM_2018_CarCode/WM_2018_CarCode.ino
+++ Onboard/WM_2018_CarCode/WM_2018_CarCode.ino
@@ -1,10 +1,24 @@
+/* --- Constants --- */
+// sensor value of no magnetic field
+#define NO_FIELD = 505L
+// Magnetic sensor threshold
+#define MAG_THRESH = 10L
+
+
+
 /* ---- Basic Variables ---- */
 // Mode Variables
 boolean testing = false;
 
+// number of over threshold hall readings
+int readings;
+unsigned long startTime;
+unsigned long endTime;
+int hallCount = 0;
+
 //Pin Definition
 int USBSerial = 57600;  // Speed of USB serial communications
-
+int hallSensorPin = 0; // pin for hall sensor
 /* ---- Initial Setup Code ---- */
 void setup() {
   //Pin Definition
@@ -17,10 +31,45 @@ void setup() {
   }
 }
 
+// returns 0 for no field, negative for south pole, positive for north pole
+int hallSense(){
+  // sensor measurement (add correct input port)
+  int raw = analogRead(hallSensorPin);
+
+  long scaledMeasurement = raw - NO_FIELD;
+
+  if(scaledMeasurement < MAG_THRESH && scaledMeasurement > -MAG_THRESH){
+    return 0;
+  } else {
+    return scaledMeasurement;
+  }
+  
+}
+
 /* ---- Additional Functions ---- */
 // Function handeling temperature sensor data collection
 void temperature() {
   
+}
+
+// functions for getting rpm from hall sensor
+void logHallSensor(){
+  if(hallSense != 0){
+    readings++;
+  }
+  hallCount++;
+}
+void resetRPM(){
+  hallCount = 0;
+  startTime = millis();  
+}
+
+int calculateRPM() {
+  endTime = millis();
+  double elapsedTime = (endTime - startTime) / 1000.0;
+  double rounds = readings / 2;
+  double roundsPerSecond = rounds/elapsedTime;
+  return (int) roundsPerSecond * 3600;  
 }
 
 // Function handeling serial (USB) communicatoin
